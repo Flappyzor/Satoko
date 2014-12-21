706f070feb3ca4994ccd8a2579a3ba32555d4aa3
Flashwave <julianvdg@gmail.com>
2014-12-21 17:36:02 +0100

    More database stuff

--- lib/SatokoDatabase.php
+++ lib/SatokoDatabase.php
@@ -4,3 +4,11 @@
 	(c)Flashwave <http://flash.moe>
 	Distributed under the MIT-License
 */
+
+// Include database driver
+if(file_exists('database/' . $satoko['db']['driver'] . '.php')) {
+    // Require database library
+    require_once 'database/' . $satoko['db']['driver'] . '.php';
+} else {
+    die('<h1>Failed to load database driver.</h1>Satoko depends on a working SQL library, without one it cannot function.<hr />Satoko Imageboard ' . SATOKO_VERSION);
+}
--- lib/satoko.php
+++ lib/satoko.php
@@ -5,5 +5,10 @@
     Distributed under the MIT-License
 */
 
+// Define Satoko Version
+define('SATOKO_VERSION', '1.0alpha1');
+
+// Include libraries
 require_once 'SatokoBoard.php';
+require_once 'SatokoDatabase.php';
 require_once 'SatokoTemplates.php';
