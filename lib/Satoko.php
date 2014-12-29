b4c9182a5f14a9e694289817427b0c28f7a411e5
Flashwave <julianvdg@gmail.com>
2014-12-29 15:04:00 +0100

    Updates

--- lib/Satoko.php
+++ lib/Satoko.php
@@ -6,19 +6,30 @@
 */
 
 // Define Satoko Version
-define('SATOKO_VERSION', '1.0alpha1');
+define('SATOKO_VERSION', '1.0alpha1'); // Version number for update checking and stuff like that.
+define('SATOKO_ROOT_DIRECTORY', str_replace('lib', '', dirname(__FILE__))); // Might want to look if there's a better way to do this.
+define('PHPEXT', substr(strrchr(__FILE__, '.'), 1)); // PHP extension because some people like to alter it?
 
 // Include configuration
-require_once '../.config.php';
+require_once SATOKO_ROOT_DIRECTORY . '.config.' . PHPEXT;
+
+// Error Reporting
+if($satoko['exposeErrors']) {
+    error_reporting(-1);
+    @ini_set('display_errors', true);
+} else {
+    error_reporting(0);
+    @ini_set('display_errors', false);
+}
 
 // Include libraries
-require_once 'SatokoBoard.php';
-require_once 'SatokoTemplates.php';
+require_once 'SatokoBoard.' . PHPEXT;
+require_once 'SatokoTemplates.' . PHPEXT;
 
 // Include database driver
-if(file_exists('database/' . $satoko['db']['driver'] . '.php')) {
+if(file_exists(SATOKO_ROOT_DIRECTORY . 'lib/database/' . $satoko['db']['driver'] . '.' . PHPEXT)) {
     // Require database library
-    require_once 'database/' . $satoko['db']['driver'] . '.php';
+    require_once SATOKO_ROOT_DIRECTORY . 'lib/database/' . $satoko['db']['driver'] . '.' . PHPEXT;
 } else {
     die('<h1>Failed to load database driver.</h1>Satoko depends on a working SQL library, without one it cannot function.<hr />Satoko Imageboard ' . SATOKO_VERSION);
 }
