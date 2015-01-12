f46be9afabf5b0b9a9c8456cee22a187dab813eb
Flashwave <julianvdg@gmail.com>
2015-01-12 17:11:04 +0100

    4 spaces > tab and shorter shit

--- logo/image.php
+++ logo/image.php
@@ -6,24 +6,17 @@ $printURI = false;
 
 // Put images in the same folder as this script and this script will print out a random filename including the full URI to it.
 
-$images		= array();								// Define array on $images to "store" images in
-$imageTypes	= array('gif', 'jpg', 'jpeg', 'png');	// Define allowed file formats
-
-foreach(glob('*.*') as $data) { // Run foreach to check if data in folder is actually images
-	if(in_array(str_replace('.', '', strstr($data, '.')), $imageTypes)) {
-		$images[] = $data; // Add images to array
-	}
-}
+$images = glob('*.{gif,jpg,jpeg,png}', GLOB_BRACE); // Get image with specified extensions from the same folder
 
 // Assign a random value from the generated array to a variable
 $image = $images[array_rand($images)];
 
 if($printURI) {
-	// Print out a properly formatted URI
-	print (empty($_SERVER['HTTPS']) ? 'http://' : 'https://') . $_SERVER['SERVER_NAME'] . str_replace(basename(__FILE__), null, $_SERVER['PHP_SELF']) . $image;
+    // Print out a properly formatted URI
+    print (empty($_SERVER['HTTPS']) ? 'http://' : 'https://') . $_SERVER['SERVER_NAME'] . str_replace(basename(__FILE__), null, $_SERVER['PHP_SELF']) . $image;
 } else {
-	// Set proper file header
-	header('Content-Type: ' . finfo_file(finfo_open(FILEINFO_MIME_TYPE), $image));
-	// Print out contents of the image
-	print file_get_contents($image);
+    // Set proper file header
+    header('Content-Type: ' . finfo_file(finfo_open(FILEINFO_MIME_TYPE), $image));
+    // Print out contents of the image
+    print file_get_contents($image);
 }
