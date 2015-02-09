8d5247789288aab920e08ba08834fedfae76325b
Julian van de Groep <julianvdg@gmail.com>
2015-02-09 09:42:49 +0100

    trigger_error() instead of die()

--- lib/SatokoBoard.php
+++ lib/SatokoBoard.php
@@ -7,17 +7,21 @@
 
 namespace Satoko;
 
-class Board {
+class Base {
 
+    // Class Variables
 	public static $_CONF;
 	public static $_DB;
 	public $_TPL;
 
+    // Setting Variables
+    public static $_BOARDID;
+    
 	// Constructor
 	function __construct($config) {
 		// Stop the execution if the PHP Version is older than 5.3.0
 		if(version_compare(phpversion(), '5.3.0', '<'))
-			die('<h3>Upgrade your PHP Version!</h3>');
+			trigger_error('<b>Satoko Init</b>: Upgrade your PHP Version to at least 5.3.0!', E_USER_ERROR);
 	
 		// Assign $config values to $_configuration
 		self::$_CONF = $config;
@@ -37,9 +41,21 @@ class Board {
 			return false;
 	}
 	
+	// Dynamically set configuration values, does not update the configuration file
+	public static function setConfig($key, $subkey, $value) {
+		if($subkey) {
+			if(!isset(self::$_CONF[$key])) {
+				self::$_CONF[$key] = array();
+			}
+			self::$_CONF[$key][$subkey] = $value;
+		} else {
+			self::$_CONF[$key] = $value;
+		}
+	}
+	
 	// Setting board to get its data
 	public function setBoard($ident) {
-	
+        self::$_BOARDID = $ident;
 	}
     
     // Getting JSON files with an array fallback
@@ -56,17 +72,17 @@ class Board {
     }
     
     // Initialise Twig
-    public function initTwig() {
-        // Assign default values set in the configuration
-        $templateName       = self::getConfig('tplName');
-        $templatesFolder    = self::getConfig('tplFolder');
+    public function initTwig($templateName = null, $templatesFolder = null) {
+        // Assign default values set in the configuration if $templateName and $templatesFolder are null
+        $templateName       = is_null($templateName)    ? self::getConfig('tplName')    : $templateName;
+        $templatesFolder    = is_null($templatesFolder) ? self::getConfig('tplFolder')  : $templatesFolder;
         
         // Initialise Twig Filesystem Loader
         $twigLoader = new \Twig_Loader_Filesystem(SATOKO_ROOT_DIRECTORY. $templatesFolder .'/'. $templateName .'/templates');
 
         // And now actually initialise the templating engine
         $this->_TPL = new \Twig_Environment($twigLoader, array(
-           // 'cache' => SATOKO_ROOT_DIRECTORY. $satoko['cacheFolder']
+           // 'cache' => SATOKO_ROOT_DIRECTORY. self::getConfig('cacheFolder')
         ));
         
         // Load String template loader
@@ -74,10 +90,10 @@ class Board {
     }
     
     // Get Stylesheets file from template
-    public function getStylesheets() {
-        // Assign default values set in the configuration
-        $templateName       = self::getConfig('tplName');
-        $templatesFolder    = self::getConfig('tplFolder');
+    public function getStylesheets($templateName = null, $templatesFolder = null) {
+        // Assign default values set in the configuration if $templateName and $templatesFolder are null
+        $templateName       = is_null($templateName)    ? self::getConfig('tplName')    : $templateName;
+        $templatesFolder    = is_null($templatesFolder) ? self::getConfig('tplFolder')  : $templatesFolder;
         
         // Set path to styles.json
         $stylesJSON = SATOKO_ROOT_DIRECTORY. $templatesFolder .'/'. $templateName .'/styles.json';
