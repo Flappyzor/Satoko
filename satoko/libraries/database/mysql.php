039acc660342a28dee2e8bee81fc4e83f3faabc6
Flashwave <me@flash.moe>
2015-03-14 18:23:51 +0100

    prepare for more commits

--- satoko/libraries/database/mysql.php
+++ satoko/libraries/database/mysql.php
@@ -19,67 +19,207 @@ class Database {
     public static $sql;
 
     // Constructor
-    function __construct() {
+    public static function init() {
+        
         if(!extension_loaded('PDO')) {
             // Return error and die
             trigger_error('<b>SQL Driver</b>: PDO extension not loaded.', E_USER_ERROR);
         }
 
         // Initialise connection
-        $this->initConnect(
+        self::initConnect(
             (
-            Base::getConfig('db', 'unixsocket') ?
-                $this->prepareSock(
-                    Base::getConfig('db', 'host'),
-                    Base::getConfig('db', 'database')
+            Board::getLocalConfig('db', 'unixsocket') ?
+                self::prepareSock(
+                    Board::getLocalConfig('db', 'host'),
+                    Board::getLocalConfig('db', 'database')
                 ) :
-                $this->prepareHost(
-                    Base::getConfig('db', 'host'),
-                    Base::getConfig('db', 'database'),
+                self::prepareHost(
+                    Board::getLocalConfig('db', 'host'),
+                    Board::getLocalConfig('db', 'database'),
                     (
-                        Base::getConfig('db', 'port') !== null ?
-                        Base::getConfig('db', 'port') :
+                        Board::getLocalConfig('db', 'port') !== null ?
+                        Board::getLocalConfig('db', 'port') :
                         3306
                     )
                 )
             ),
-            Base::getConfig('db', 'username'),
-            Base::getConfig('db', 'password')
+            Board::getLocalConfig('db', 'username'),
+            Board::getLocalConfig('db', 'password')
         );
+        
     }
 
     // Regular IP/Hostname connection method prepare function
-    private function prepareHost($dbHost, $dbName, $dbPort = 3306) {
+    private static function prepareHost($dbHost, $dbName, $dbPort = 3306) {
+        
         $DSN = 'mysql:host=' . $dbHost . ';port=' . $dbPort . ';dbname=' . $dbName;
 
         return $DSN;
+        
     }
 
     // Unix Socket connection method prepare function
-    private function prepareSock($dbHost, $dbName) {
+    private static function prepareSock($dbHost, $dbName) {
+        
         $DSN = 'mysql:unix_socket=' . $dbHost . ';dbname=' . $dbName;
 
         return $DSN;
+        
     }
 
     // Initialise connection using default PDO stuff
-    private function initConnect($DSN, $dbUname, $dbPword) {
+    private static function initConnect($DSN, $dbUname, $dbPword) {
+        
         try {
             // Connect to SQL server using PDO
             self::$sql = new PDO($DSN, $dbUname, $dbPword);
         } catch(PDOException $e) {
             // Catch connection errors
-            trigger_error("SQL Driver: " . $e->getMessage(), E_USER_ERROR);
+            trigger_error('SQL Driver: '. $e->getMessage(), E_USER_ERROR);
         }
+        
         return true;
+        
     }
 
     // Fetch array from database
-    public function fetchAll($table) {
-        $query = self::$sql->prepare('SELECT * FROM `' . Base::getConfig('db', 'prefix') . $table . '`');
-        $query->execute();
-        $result = $query->fetchAll(PDO::FETCH_BOTH);
+    public static function fetch($table, $fetchAll = true, $data = null) {
+        
+        // Begin preparation of the statement
+        $prepare = 'SELECT * FROM `' . Board::getLocalConfig('db', 'prefix') . $table . '`';
+        
+        // If $data is set and is an array continue
+        if(is_array($data)) {
+            
+            $prepare .= ' WHERE';
+            
+            foreach($data as $key => $value) {
+                $prepare .= ' `'. $key .'` '. $value[1] .' :'. $key .($key == key(array_slice($data, -1, 1, true)) ? ';' : ' AND');
+            }
+            
+        }
+        
+        // Actually prepare the preration
+        $query = self::$sql->prepare($prepare);
+        
+        // Bind those parameters if $data is an array that is
+        if(is_array($data)) {
+            foreach($data as $key => $value) {
+                $query->bindParam(':'. $key, $value[0]);
+            
+                // Unset variables to be safe
+                unset($key);
+                unset($value);
+            }
+        }
 
+        // Execute the prepared statements with parameters bound
+        $query->execute();
+        
+        // Do fetch or fetchAll
+        if($fetchAll)
+            $result = $query->fetchAll(PDO::FETCH_BOTH);
+        else
+            $result = $query->fetch(PDO::FETCH_BOTH);
+        
+        // And return the output
         return $result;
+        
     }
+    
+    // Insert data to database
+    public static function insert($table, $data) {
+        
+        // Begin preparation of the statement
+        $prepare = 'INSERT INTO `' . Board::getLocalConfig('db', 'prefix') . $table . '` ';
+        
+        // Run the foreach statement twice for (`stuff`) VALUES (:stuff)
+        for($i = 0; $i < 2; $i++) {
+            
+            $prepare .= '(';
+            
+            // Do more shit, don't feel like describing this so yeah
+            foreach($data as $key => $value) {
+                $prepare .= ($i ? ':' : '`') . $key . ($i ? '' : '`') . ($key == key(array_slice($data, -1, 1, true)) ? '' : ', ');
+            }
+            
+            $prepare .= ')' . ($i ? ';' : ' VALUES ');
+            
+        }
+        
+        // Actually prepare the preration
+        $query = self::$sql->prepare($prepare);
+        
+        // Bind those parameters
+        foreach($data as $key => $value) {
+            $query->bindParam(':'. $key, $value);
+            
+            // Unset variables to be safe
+            unset($key);
+            unset($value);
+        }
+
+        // Execute the prepared statements with parameters bound
+        $result = $query->execute();
+        
+        // Return whatever can be returned
+        return $result;
+        
+    }
+    
+    // Update data in the database
+    public static function update($table, $data) {
+        
+        // Begin preparation of the statement
+        $prepare = 'UPDATE `' . Board::getLocalConfig('db', 'prefix') . $table . '`';
+        
+        // Run a foreach on $data and complete the statement
+        foreach($data as $key => $values) {
+            
+            // Append WHERE or SET depending on where we are
+            $prepare .= ' '. ($key ? 'WHERE' : 'SET');
+            
+            // Do this complicated shit, I barely know what's going on anymore but it works
+            foreach($values as $column => $column_data) {
+                $prepare .= ' `'. $column .'` '. ($key ? $column_data[1] : '=') .' :'. ($key ? 'w' : 's') .'_'. $column . ($column == key(array_slice($values, -1, 1, true)) ? ($key ? ';' : '') : ($key ? ' AND' : ','));
+            }
+            
+        }
+        
+        // Actually prepare the preration
+        $query = self::$sql->prepare($prepare);
+
+        // Seperate the foreaches for the SET and WHERE clauses because it's fucking it up for some odd reason
+        // Bind Set Clauses
+        foreach($data[0] as $key => $value) {
+            // Do the binding
+            $query->bindParam(':s_'. $key, $value);
+            
+            // Unset variables to be safe
+            unset($key);
+            unset($value);
+        }
+        // Bind Where Clauses
+        foreach($data[1] as $key => $values) {
+            // Assign the array entry to a variable because fuck strict standards
+            $value = $values[0];
+            
+            // Binding two electrifying memes
+            $query->bindParam(':w_'. $key, $value);
+            
+            // Unset variables to be safe
+            unset($key);
+            unset($value);
+            unset($values);
+        }
+        
+        // Execute the prepared statements with parameters bound
+        $result = $query->execute();
+        
+        // Return whatever can be returned
+        return $result;
+        
+    }
+    
 }
