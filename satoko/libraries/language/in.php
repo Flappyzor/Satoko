a5f9fd929034c4cb1b4cfb94a3f4484970ef825d
Lawrence A. <malwareinform@gmail.com>
2015-03-15 22:04:57 +0800

    fkuc

---
+++ satoko/libraries/language/in.php
@@ -0,0 +1,35 @@
+<?php
+/*
+ * Satoko Language File
+ * Bahasa Indonesia
+ * By Flappyzor http://flappy.club/
+ */
+
+$lang = array(
+
+    'catalog'       => 'Katalog',
+    'refresh'       => 'Menyegarkan',
+    'style'         => 'Gaya',
+    'home'          => 'Rumah',
+    'manage'        => 'Mengurus',
+    
+    'name'          => 'Nama',
+    'email'         => 'E-mail',
+    'options'       => 'Pilihan',
+    'subject'       => 'Pokok',
+    'comment'       => 'Komentar',
+    'submit'        => 'Mengajukan',
+    'verification'  => 'Verifikasi',
+    'file'          => 'Berkas',
+    'spoilerq'      => 'Spoiler?',
+    'nofileq'       => 'Tidak Berkas?',
+    'password'      => 'Password',
+    'password_sub'  => '(digunakan huruf untuk pemindahan)',
+    
+    'deletePost'    => 'Menghapuskan Huruf',
+    'fileOnly'      => 'Catatan Hanya',
+    
+    'prev'          => 'Sebelumnya',
+    'next'          => 'Berikutnya'
+
+);
